66adec79626e8e2fd1e490555ed7f90a70d0eb49
muhrizfa <muhammadrizfa@gmail.com>
2019-05-29 13:38:09 +0700

    Set Card Box

--- lib/main.dart
+++ lib/main.dart
@@ -27,17 +27,71 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    final children = <Widget>[];
-    for (var i = 0; i < 10; i++) {
-      children.add(new ListTile());
-    }
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
         centerTitle: true,
       ),
-      body: ListView(
-        children: children,
+      body: Container(
+        margin: EdgeInsets.all(10),
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          elevation: 5,
+          child: Container(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Container(
+                    width: double.infinity,
+                    child: ClipRRect(
+                      borderRadius:
+                          BorderRadius.vertical(top: Radius.circular(10.0)),
+                      child: Image.network(
+                        'https://picsum.photos/250?image=9',
+                        fit: BoxFit.cover,
+                      ),
+                    )),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              "Sierra",
+                              style: TextStyle(fontSize: 25),
+                            ),
+                            Text(
+                              "20 September 2016",
+                              style: TextStyle(
+                                  color: Colors.black54, fontSize: 15),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Text(
+                        "10.12",
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                  child: Text(
+                      "Justo amet duo est dolor voluptua amet, diam lorem vero no aliquyam diam et kasd, magna nonumy dolor duo at."),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
